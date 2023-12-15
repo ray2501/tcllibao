d178b9be16bad756ec8b978615235ee2e0ae9a04
Danilo Chang <ray2501@gmail.com>
2023-12-15 10:37:52 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded libao 0.3 \
-    [list load [file join $dir liblibao0.3.so] libao]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded libao 0.3 \
+	    [list load [file join $dir libtcl9libao0.3.so] [string totitle libao]]
+} else {
+    package ifneeded libao 0.3 \
+	    [list load [file join $dir liblibao0.3.so] [string totitle libao]]
+}
