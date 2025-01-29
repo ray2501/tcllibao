b1278156936bbe5c6da93cd7018e25930230e023
Danilo Chang <ray2501@gmail.com>
2025-01-29 11:11:48 +0800

    Setup version to 0.5

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded libao 0.4 \
-	    [list load [file join $dir libtcl9libao0.4.so] [string totitle libao]]
+    package ifneeded libao 0.5 \
+	    [list load [file join $dir libtcl9libao0.5.so] [string totitle libao]]
 } else {
-    package ifneeded libao 0.4 \
-	    [list load [file join $dir liblibao0.4.so] [string totitle libao]]
+    package ifneeded libao 0.5 \
+	    [list load [file join $dir liblibao0.5.so] [string totitle libao]]
 }
--- tcllibao_SUSE.spec
+++ tcllibao_SUSE.spec
@@ -4,7 +4,7 @@
 
 Name:          tcllibao
 Summary:       Tcl bindings for libao
-Version:       0.4
+Version:       0.5
 Release:       0
 License:       GPL-2.0-or-later
 Group:         Development/Libraries/Tcl
